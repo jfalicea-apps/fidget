f50ec706b194fa838a6cd1d2eb04655df1a9a3a2
Jonathan <jonathan@boldideas.net>
2021-02-15 00:15:02 -0500

    adds header information and helmet

--- app.js
+++ app.js
@@ -2,11 +2,21 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
+const helmet = require('helmet');
 var buttonRouter = require('./services/translateButton/buttonTranslation.routes');
 var usersRouter = require('./services/users/users.route');
 const securityRouter = require('./services/auth/checkSecKey');
 var app = express();
+app.use(helmet());
+app.use(function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET,HEAD,OPTIONS,POST,PUT');
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+  );
+  next();
+});
 
 app.use(logger('dev'));
 app.use(express.json());
